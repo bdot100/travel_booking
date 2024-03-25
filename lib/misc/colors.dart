19e87efeabc3513f2f7f41ffaa2095ffd959d8c3
bdot100 <bidemiokunade@gmail.com>
2024-03-25 21:05:37 +0100

    Home Slider Done

---
+++ lib/misc/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors{
+  static const Color textColor1 = Color(0xFF989ACD);
+  static const Color textColor2 = Color(0xFF878593);
+  static const Color bigTextColor = Color(0xFF2E2E31);
+  static const Color mainColor = Color(0xFF5d69b3);
+  static const Color startColor = Color(0xFFE7BB4E);
+  static const Color mainTextColor = Color(0xFFababad);
+  static const Color buttonBackground = Color(0xFFF1F2);
+}
---
+++ lib/widgets/responsive_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:travel_booking/misc/colors.dart';
+
+// ignore: must_be_immutable
+class ResponsiveButton extends StatelessWidget {
+  bool? isResponsive;
+  double? width;
+  ResponsiveButton({super.key, this.isResponsive=false, this.width});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: AppColors.mainColor
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("img/button.webp")
+        ],
+      ),
+    );
+  }
+}
