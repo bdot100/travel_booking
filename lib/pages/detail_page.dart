2d184b03657fdbe856db083cb32270e6379e3fd7
bdot100 <bidemiokunade@gmail.com>
2024-04-24 13:06:17 +0100

    Home page and detail page created

---
+++ lib/pages/detail_page.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:travel_booking/misc/colors.dart';
+import 'package:travel_booking/widgets/app_large_text.dart';
+import 'package:travel_booking/widgets/app_text.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({super.key});
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  int gottenStars = 4;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              right: 0,
+              child: Container(
+                width: double.maxFinite,
+                height: 350,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("img/mountaining.jpeg"),
+                    fit: BoxFit.cover
+                  )
+                ),
+              )
+            ),
+            Positioned(
+              left: 20,
+              top: 50,
+              child: Row(
+              children: [
+                IconButton(onPressed: (){}, icon: Icon(Icons.menu),
+                color: Colors.white,
+                ),
+                // IconButton(onPressed: (){}, icon: Icon(Icons.menu),)
+                ],
+              )
+            ),
+            Positioned(
+              top: 320,
+              child: Container(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+                width: MediaQuery.of(context).size.width,
+                height: 500,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30)
+                  )
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        AppLargeText(text: "Yosemite", color: Colors.black.withOpacity(0.8),),
+                        AppLargeText(text: "\$ 250", color: AppColors.mainColor,)
+                      ],
+                    ),
+                    SizedBox(height: 10,),
+                    Row(
+                      children: [
+                        Icon(Icons.location_on, color: AppColors.mainColor,),
+                        SizedBox(width: 5,),
+                        AppText(text: "USA, California", color: AppColors.textColor1,)
+                      ],
+                    ),
+                    SizedBox(height: 20,),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(5, (index) {
+                            return Icon(Icons.star, color: index < gottenStars ?AppColors.startColor : AppColors.textColor2,);
+                          }),
+                        ),
+                        SizedBox(width: 10,),
+                        AppText(text: "(4.0)", color: AppColors.textColor2,)
+                      ],
+                    ),
+                    SizedBox(height: 25,),
+                    AppLargeText(text: "People", color: Colors.black.withOpacity(0.8),size: 20,),
+                    SizedBox(height: 5,),
+                    AppText(text: "Number of people in your group", color: AppColors.mainTextColor,)
+                  ],
+                ),
+              )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/nav_pages/main_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:travel_booking/pages/nav_pages/bar_item_page.dart';
+import 'package:travel_booking/pages/home_page.dart';
+import 'package:travel_booking/pages/nav_pages/my_page.dart';
+import 'package:travel_booking/pages/nav_pages/search_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List pages = [
+    HomePage(),
+    BarItemPage(),
+    SearchPage(),
+    MyPage()
+  ];
+
+  int currentIndex=0;
+
+  void onTap(int index){
+    setState(() {
+      currentIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedFontSize: 0,
+        selectedFontSize: 0,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        onTap: onTap,
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.black54,
+        unselectedItemColor: Colors.grey.withOpacity(0.5),
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        elevation: 0,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.apps), label: 'Home',),
+          BottomNavigationBarItem(icon: Icon(Icons.bar_chart_sharp), label: 'Bar'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'My page')
+        ],
+      ),
+    );
+  }
+}
